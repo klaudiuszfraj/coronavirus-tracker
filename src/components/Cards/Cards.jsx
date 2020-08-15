a3faf78f896352ba4dcf63d9c4faa5197b2207bd
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-08-15 12:46:40 +0200

    correct color property error

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -12,25 +12,25 @@ function Cards(props) {
     <Grid container spacing={3} justify='center'>
         <Grid item component={Card}>
             <CardContent>
-                <Typography color='textSecondary ' gutterBottom>Infected</Typography>
+                <Typography color='textSecondary' gutterBottom>Infected</Typography>
                 <Typography variant='h5'>Real Data</Typography>
-                <Typography color="textSecondary"> real date</Typography>
+                <Typography color='textSecondary'> real date</Typography>
                 <Typography variant='body2'>Number of active cases of COVID-19</Typography>
             </CardContent>
         </Grid>
         <Grid item component={Card}>
             <CardContent>
-                <Typography color='textSecondary ' gutterBottom>Recovered</Typography>
+                <Typography color='textSecondary' gutterBottom>Recovered</Typography>
                 <Typography variant='h5'>Real Data</Typography>
-                <Typography color="textSecondary"> real date</Typography>
+                <Typography color='textSecondary'> real date</Typography>
                 <Typography variant='body2'>Number of recoveries from COVID-19</Typography>
             </CardContent>
         </Grid>
         <Grid item component={Card}>
             <CardContent>
-                <Typography color='textSecondary ' gutterBottom>Deaths</Typography>
+                <Typography color='textSecondary' gutterBottom>Deaths</Typography>
                 <Typography variant='h5'>Real Data</Typography>
-                <Typography color="textSecondary"> real date</Typography>
+                <Typography color='textSecondary'> real date</Typography>
                 <Typography variant='body2'>Number of deaths caused by COVID-19</Typography>
             </CardContent>
         </Grid>

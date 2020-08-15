3b5d1d631e78b641795b9a80c3f8923538c22a5d
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-08-15 13:26:24 +0200

    add couters and set data

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,42 +1,62 @@
-import React, { useState } from 'react';
-import { Card, CardContent, Typography, Grid} from "@material-ui/core";
+import React, {useState} from 'react';
+import {Card, CardContent, Typography, Grid} from "@material-ui/core";
+import CountUp from "react-countup";
 
 import style from './Cards.module.css'
 
 
-function Cards(props) {
-    console.log(props)
+function Cards({data: {confirmed, recovered, deaths, lastUpdate}}) {
+    console.log(confirmed);
+    if (!confirmed) {
+        return 'Loading...';
+    }
 
- return (
-  <div className={style.container}>
-    <Grid container spacing={3} justify='center'>
-        <Grid item component={Card}>
-            <CardContent>
-                <Typography color='textSecondary' gutterBottom>Infected</Typography>
-                <Typography variant='h5'>Real Data</Typography>
-                <Typography color='textSecondary'> real date</Typography>
-                <Typography variant='body2'>Number of active cases of COVID-19</Typography>
-            </CardContent>
-        </Grid>
-        <Grid item component={Card}>
-            <CardContent>
-                <Typography color='textSecondary' gutterBottom>Recovered</Typography>
-                <Typography variant='h5'>Real Data</Typography>
-                <Typography color='textSecondary'> real date</Typography>
-                <Typography variant='body2'>Number of recoveries from COVID-19</Typography>
-            </CardContent>
-        </Grid>
-        <Grid item component={Card}>
-            <CardContent>
-                <Typography color='textSecondary' gutterBottom>Deaths</Typography>
-                <Typography variant='h5'>Real Data</Typography>
-                <Typography color='textSecondary'> real date</Typography>
-                <Typography variant='body2'>Number of deaths caused by COVID-19</Typography>
-            </CardContent>
-        </Grid>
-    </Grid>
+    return (
+        <div className={style.container}>
+            <Grid container spacing={3} justify='center'>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color='textSecondary' gutterBottom>Infected</Typography>
+                        <Typography variant='h5'>
+                            <CountUp end={confirmed.value}
+                                     start={0}
+                                     duration={2.5}
+                                     separator={','}/>
+                        </Typography>
+                        <Typography color='textSecondary'>{new Date(lastUpdate).toLocaleDateString()}</Typography>
+                        <Typography variant='body2'>Number of active cases of COVID-19</Typography>
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color='textSecondary' gutterBottom>Recovered</Typography>
+                        <Typography variant='h5'>
+                            <CountUp end={recovered.value}
+                                     start={0}
+                                     duration={2.5}
+                                     separator={','}/>
+                        </Typography>
+                        <Typography color='textSecondary'>{new Date(lastUpdate).toLocaleDateString()}</Typography>
+                        <Typography variant='body2'>Number of recoveries from COVID-19</Typography>
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color='textSecondary' gutterBottom>Deaths</Typography>
+                        <Typography variant='h5'>
+                            <CountUp end={deaths.value}
+                                     start={0}
+                                     duration={2.5}
+                                     separator={','}/>
+                        </Typography>
+                        <Typography color='textSecondary'>{new Date(lastUpdate).toLocaleDateString()}</Typography>
+                        <Typography variant='body2'>Number of deaths caused by COVID-19</Typography>
+                    </CardContent>
+                </Grid>
+            </Grid>
 
-  </div>
- );
+        </div>
+    );
 }
+
 export default Cards;

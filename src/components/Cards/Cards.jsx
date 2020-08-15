bff1cc758532bdbb1f4a06043482d4588eedd43d
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-08-15 09:30:04 +0200

    impotr components

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+
+function Cards() {
+
+
+ return (
+  <div>
+   <h1>Cards</h1>
+  </div>
+ );
+}
+export default Cards;
---
+++ src/components/Chart/Chart.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+
+function Chart() {
+ return (
+  <div>
+   <h1>Chart</h1>
+  </div>
+ );
+}
+export default Chart;

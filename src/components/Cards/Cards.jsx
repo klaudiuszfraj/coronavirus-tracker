f8e69ca8d7c37bf910e737bf46840d852597db54
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-08-15 13:45:31 +0200

    remove import useState

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {Card, CardContent, Typography, Grid} from "@material-ui/core";
 import CountUp from "react-countup";
 import cx from 'classnames';

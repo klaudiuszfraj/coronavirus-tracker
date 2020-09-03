cf3a2432340293fd30a2c72bfe3fff7520036814
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-09-03 13:07:41 +0200

    fix issue back to global 2

--- src/App.js
+++ src/App.js
@@ -33,8 +33,8 @@ class App extends Component {
             data: fetchedCountryData,
             country: country,
             mapCenter: {
-                lat: fetchedCountryData.countryInfo.lat,
-                lng: fetchedCountryData.countryInfo.long
+                lat: country==='global' ? 52 : fetchedCountryData.countryInfo.lat,
+                lng: country==='global' ? 20 : fetchedCountryData.countryInfo.long
             }
         })
     }

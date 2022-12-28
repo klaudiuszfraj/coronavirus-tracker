617b74ece4e4609b878fd14f126caf180192636c
Klaudiusz Frajkowski <kfrajkowski@egnyte.com>
2022-12-28 12:44:33 +0100

    removed unsuported api

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-const API_URL1 = 'https://covid19.mathdro.id/api';
 const API_URL = 'https://disease.sh/v3/covid-19';
 
 
@@ -19,22 +18,6 @@ export const fetchData = async (country= "global")=>{
         console.log(error);
     }
 };
-export const fetchDailyDate = async ()=>{
-    try {
-        const { data } = await axios.get(`${API_URL1}/daily`)
-        console.log('fetch data daily api',data);
-
-        const mofifiadData = data.map(daileData => ({
-            confirmed: daileData.confirmed.total,
-            deaths: daileData.deaths.total,
-            date: daileData.reportDate
-        }));
-
-        return mofifiadData;
-    } catch (error){
-        console.log(error);
-    }
-}
 
 export const fetchCountriesInfo = async ()=>{
     try {
